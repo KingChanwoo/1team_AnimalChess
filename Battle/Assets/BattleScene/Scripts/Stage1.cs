2cf90cc1f359fc852583bb99f0a403fad7ee4885
조승희 <SeungHee30917@gmail.com>
2021-01-30 14:37:45 +0900

    적 3D모델 수정

--- Battle/Assets/BattleScene/Scripts/Stage1.cs
+++ Battle/Assets/BattleScene/Scripts/Stage1.cs
@@ -16,8 +16,8 @@ public class Stage1 : MonoBehaviour
             // 원하는 유닛 수 만큼 복사해서 사용
             // 본래 능력치의 몇 %로 구현할 것인지 입력하여 사용(100% = 100)
             // 123~ 141
-            aiOpponent.AddEnemy(125, 1, 2,100);
-            aiOpponent.AddEnemy(125, 5, 2,100);
+            aiOpponent.AddEnemy(0, 1, 2,100); //125
+            aiOpponent.AddEnemy(0, 5, 2,100); //125
         }
         else if (r == 2)
         {
